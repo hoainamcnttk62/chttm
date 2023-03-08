21e5e2edf38cb0b7bf66880ef96cef5cfc3c5446
namlor08 <114592173+hoainamcnttk62@users.noreply.github.com>
2023-03-08 13:24:34 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from rewrite_parrot import GPTRewriteParrot
-with open('input.txt', 'r') as file:
-    content = file.read()
-p = GPTRewriteParrot()
-output = p.paraphrase(content)
-with open('output.txt', 'w') as file:
-    file.write(output)
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
